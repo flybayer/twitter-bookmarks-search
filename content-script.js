a659c7ce565f7f5588634728bb42c183cc4b7e20
Brandon Bayer <b@bayer.ws>
2019-11-07 20:22:12 -0800

    search box in place

--- content-script.js
+++ content-script.js
@@ -1,17 +1,67 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import delay from "delay"
 
 console.log("Hello from nextjs content script!")
 
+async function onClick(event) {
+  event.preventDefault()
+}
+
+const getTimeline = () => document.querySelector('[aria-label="Timeline: Bookmarks"]')
+const getSearchBox = () => document.querySelector('[role="search"]')
+
+const css = `
+  #tbs-root form > div > div {
+    background: white;
+    border: 1px solid #21a1f3;
+    border-radius: 9999px;
+  }
+`
+
 function Content() {
+  const [active, setActive] = React.useState(false)
   return (
-    <div style={{ background: "pink" }}>
-      <h1>Hello world - My first Extension</h1>
+    <div style={{ padding: "8px 15px" }}>
+      <style>{active ? css : null}</style>
+      <SearchBox onSubmit={onClick} onClick={() => setActive(true)} />
     </div>
   )
 }
 
-const app = document.createElement("div")
-app.id = "next-extension-root"
-document.body.appendChild(app)
-ReactDOM.render(<Content />, app)
+function SearchBox({ onSubmit, onClick }) {
+  const ref = React.useRef()
+
+  React.useEffect(() => {
+    ;(async function() {
+      let searchBox = getSearchBox()
+      while (!searchBox) {
+        await delay(10)
+        searchBox = getSearchBox()
+      }
+
+      const newSearchBox = searchBox.cloneNode(true)
+      newSearchBox.onsubmit = onSubmit
+      newSearchBox.onclick = onClick
+      ref.current.append(newSearchBox)
+
+      const input = document.querySelector("#tbs-root input")
+      input.placeholder = "Search Bookmarks"
+    })()
+  }, [onSubmit])
+
+  return <div ref={ref} />
+}
+
+;(async function() {
+  let timeline = getTimeline()
+  while (!timeline) {
+    await delay(10)
+    timeline = getTimeline()
+  }
+
+  const app = document.createElement("div")
+  app.id = "tbs-root"
+  timeline.prepend(app)
+  ReactDOM.render(<Content />, app)
+})()
--- pages/popup.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function Popup() {
-  return <div>Welcome to Popup Next.js! :)</div>
-}
-
-export default Popup
