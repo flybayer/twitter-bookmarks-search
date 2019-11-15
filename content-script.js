f36224bbbc15e2dea4847125cf27892a00fe91e9
Brandon Bayer <b@bayer.ws>
2019-11-14 20:25:07 -0800

    works

--- content-script.js
+++ content-script.js
@@ -1,17 +1,159 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import delay from "delay"
+import matchSorter from "match-sorter"
+import tweet2Html from "tweet-html"
 
 console.log("Hello from nextjs content script!")
 
-async function onClick(event) {
-  event.preventDefault()
+const browser = require("webextension-polyfill")
+
+let authorization
+let csrfToken
+
+async function messageListener(message) {
+  console.log("Received message", message)
+  if (message.name === "credentials") {
+    authorization = message.authorization
+    csrfToken = message.csrfToken
+  }
+}
+
+browser.runtime.onMessage.addListener(messageListener)
+
+async function fetchBookmarks() {
+  if (!authorization) {
+    return console.log("authorization is blank")
+  }
+  if (!csrfToken) {
+    return console.log("csrfToken is blank")
+  }
+
+  console.log("Fetching bookmarks...")
+  const res = await fetch(
+    "https://api.twitter.com/2/timeline/bookmark.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&count=2000&ext=mediaStats%2ChighlightedLabel%2CcameraMoment&simple_quoted_tweet=true&include_user_entities=true",
+    {
+      credentials: "include",
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "x-twitter-active-user": "yes",
+        "x-twitter-auth-type": "OAuth2Session",
+        "x-twitter-client-language": "en",
+        "x-csrf-token": csrfToken,
+        authorization,
+      },
+      referrer: "https://twitter.com/i/bookmarks",
+      referrerPolicy: "no-referrer-when-downgrade",
+      body: null,
+      method: "GET",
+      mode: "cors",
+    }
+  )
+  const json = await res.json()
+  // console.log("RES", json)
+  return json
 }
 
 const getTimeline = () => document.querySelector('[aria-label="Timeline: Bookmarks"]')
+const getTweetsContainer = () => document.querySelector('[aria-label="Timeline: Bookmarks"] > div:last-child')
 const getSearchBox = () => document.querySelector('[role="search"]')
 
-const css = `
+function useBookmarkedTweets() {
+  const [tweets, setTweets] = React.useState(null)
+  React.useEffect(() => {
+    ;(async () => {
+      while (!authorization && !csrfToken) {
+        await delay(10)
+      }
+      let success = false
+      while (!success) {
+        try {
+          const json = await fetchBookmarks()
+          let tweets = Object.values(json.globalObjects.tweets)
+          let users = json.globalObjects.users
+          tweets = tweets.map(tweet => ({ ...tweet, user: users[tweet.user_id_str] }))
+          console.log("Loaded bookmarked tweets", tweets)
+          setTweets(tweets)
+          success = true
+        } catch (err) {
+          await delay(50)
+          console.log("[fetchBookmarks] failed. Retrying...", err)
+        }
+      }
+    })()
+  }, [])
+  return tweets
+}
+
+function Content() {
+  const [inputActive, setInputActive] = React.useState(false)
+  const [results, setResults] = React.useState(null)
+  const tweets = useBookmarkedTweets()
+
+  async function onSubmit(event) {
+    event.preventDefault()
+    if (!tweets) {
+      return alert("Oh snap, no bookmarked tweets are loaded. Reload and try again")
+    }
+
+    const query = event.target.elements[0].value
+    console.log("Query", query)
+    const results = matchSorter(tweets, query, { keys: ["full_text"] })
+    console.log("Search results", results)
+    setResults(results)
+  }
+
+  React.useEffect(() => {
+    if (results) {
+      getTimeline().style
+      getTweetsContainer().style.display = "none"
+    } else {
+      getTweetsContainer().style.display = ""
+    }
+  }, [results])
+
+  return (
+    <div>
+      <style>{css}</style>
+      <style>{inputActive ? inputActiveCss : null}</style>
+      <div style={{ padding: "8px 15px" }}>
+        <SearchBox onSubmit={onSubmit} onClick={() => setInputActive(true)} />
+        {/* <div>Bookmarks loaded: {tweets ? tweets.length : "no"}</div> */}
+        <div
+          style={{
+            margin: "4px 0",
+            fontSize: 13,
+            fontStyle: "italic",
+            textAlign: "center",
+            color: "#18bf64",
+          }}
+        >
+          Powered by{" "}
+          <a
+            href="https://acornbookmarks.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#18bf64" }}
+          >
+            Acorn Bookmark Manager
+          </a>
+        </div>
+      </div>
+      {results ? (
+        <div style={{ fontWeight: 800, fontSize: 19, margin: "12px 0 12px 74px" }}>
+          Found {results.length} results
+        </div>
+      ) : null}
+
+      {results ? results.map(tweet => <TweetResult tweet={tweet} key={tweet.id_str} />) : null}
+    </div>
+  )
+}
+
+const inputActiveCss = `
   #tbs-root form > div > div {
     background: white;
     border: 1px solid #21a1f3;
@@ -19,18 +161,81 @@ const css = `
   }
 `
 
-function Content() {
-  const [active, setActive] = React.useState(false)
+const css = `
+  #tbs-root {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif;
+  }
+
+  .tweet-result-container {
+    display: flex;
+    border-bottom: 1px solid rgb(230, 236, 240);
+    padding: 10px 0;
+    transition-property: background-color;
+    transition-duration: 0.2s;
+    padding: 15px;
+  }
+
+  // .tweet-result-container:hover {
+  //   background-color: rgb(245,248,250);
+  // }
+
+  .tweet-result-container a, .tweet-result-container a:visited {
+    color: rgb(27, 149, 224);
+    text-decoration: none;
+  }
+  .tweet-result-container a:hover {
+    text-decoration: underline;;
+  }
+
+  .tweet-result-container img:not(.emoji), .tweet-result-container video {
+    max-width: 100%;
+    border-radius: 14px;
+    max-height: 500px;
+    width: 100%;
+  }
+  .tweet-result-container iframe {
+    width: 100%;
+    height: 300px;
+  }
+  .tweet-result-container .emoji {
+    height: 1.2em;
+    width: 1.2em;
+  }
+
+  .tweet-result > a.date {
+    line-height: 1.8;
+  }
+  .tweet-result > .text {
+    margin-bottom: 10px;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+  }
+  .tweet-result *:last-child {
+    // margin-top: 10px;
+  }
+`
+
+function TweetResult({ tweet }) {
   return (
-    <div style={{ padding: "8px 15px" }}>
-      <style>{active ? css : null}</style>
-      <SearchBox onSubmit={onClick} onClick={() => setActive(true)} />
-    </div>
+    <article className="tweet-result-container">
+      <div style={{ flexBasis: 49, flexGrow: 0, flexShrink: 0, alignItems: "center", marginRight: 10 }}>
+        <img src={tweet.user.profile_image_url_https} style={{ borderRadius: 9999 }} />
+      </div>
+      <div>
+        <span style={{ fontWeight: 700 }}>{tweet.user && tweet.user.name}</span>{" "}
+        <span style={{ color: "rgb(101, 119, 134)" }}>@{tweet.user && tweet.user.screen_name}</span>
+        <div
+          className="tweet-result"
+          dangerouslySetInnerHTML={{ __html: tweet2Html(tweet, tweet.user && tweet.user.screen_name) }}
+        ></div>
+      </div>
+    </article>
   )
 }
 
 function SearchBox({ onSubmit, onClick }) {
-  const ref = React.useRef()
+  const containerRef = React.useRef()
+  const searchRef = React.useRef()
 
   React.useEffect(() => {
     ;(async function() {
@@ -43,14 +248,19 @@ function SearchBox({ onSubmit, onClick }) {
       const newSearchBox = searchBox.cloneNode(true)
       newSearchBox.onsubmit = onSubmit
       newSearchBox.onclick = onClick
-      ref.current.append(newSearchBox)
+      containerRef.current.append(newSearchBox)
+      searchRef.current = newSearchBox
 
       const input = document.querySelector("#tbs-root input")
       input.placeholder = "Search Bookmarks"
     })()
+  }, [])
+
+  React.useEffect(() => {
+    searchRef.current.onsubmit = onSubmit
   }, [onSubmit])
 
-  return <div ref={ref} />
+  return <div ref={containerRef} />
 }
 
 ;(async function() {
@@ -60,8 +270,11 @@ function SearchBox({ onSubmit, onClick }) {
     timeline = getTimeline()
   }
 
-  const app = document.createElement("div")
-  app.id = "tbs-root"
-  timeline.prepend(app)
-  ReactDOM.render(<Content />, app)
+  let root = document.querySelector("#tbs-root")
+  if (!root) {
+    root = document.createElement("div")
+    root.id = "tbs-root"
+    timeline.prepend(root)
+  }
+  ReactDOM.render(<Content />, root)
 })()
--- pages/background.js
+++ pages/background.js
@@ -1,10 +1,83 @@
+import delay from "delay"
 export default () => "Nothing to see, just a background script :)"
 
 if (typeof window !== "undefined") {
+  const browser = require("webextension-polyfill")
+
   console.log("Hello from background script!")
 
-  const browser = require("webextension-polyfill")
-  window.browser = browser
-
   browser.runtime.getPlatformInfo().then(console.log)
+
+  // Allow us to embed tweets
+  browser.webRequest.onHeadersReceived.addListener(
+    details => {
+      const headers = details.responseHeaders // original headers
+      const csp = headers.find(h => h.name.toLowerCase() === "content-security-policy")
+      if (csp) {
+        console.log("request headers", headers)
+        const urls = "https://*.twitter.com https://*.twimg.com"
+
+        csp.value = csp.value.replace("frame-src 'self'", "frame-src 'self' " + urls)
+        csp.value = csp.value.replace("img-src 'self'", "img-src 'self' " + urls)
+        csp.value = csp.value.replace(
+          "script-src 'self' 'unsafe-inline'",
+          "script-src 'self' 'unsafe-inline' " + urls
+        )
+        csp.value = csp.value.replace(
+          "style-src 'self' 'unsafe-inline'",
+          "style-src 'self' 'unsafe-inline' " + urls
+        )
+
+        console.log("csp header", csp)
+      }
+
+      // return modified headers
+      return { responseHeaders: headers }
+    },
+    {
+      urls: ["*://*.twitter.com/*"],
+    },
+    ["blocking", "responseHeaders"]
+  )
+
+  let tabId
+  let authorization
+  let csrfToken
+
+  browser.webRequest.onSendHeaders.addListener(
+    async details => {
+      tabId = details.tabId
+      authorization = details.requestHeaders.find(h => h.name.toLowerCase() === "authorization").value
+      csrfToken = details.requestHeaders.find(h => h.name.toLowerCase() === "x-csrf-token").value
+
+      sendCredentials()
+    },
+    { urls: ["*://*.twitter.com/*bookmark.json*"] },
+    ["requestHeaders"]
+  )
+
+  async function sendCredentials() {
+    let messageSent = false
+    let tries = 0
+    while (!messageSent && tries < 100) {
+      try {
+        tries++
+        console.log("Trying message...")
+        const res = await browser.tabs.sendMessage(tabId, {
+          name: "credentials",
+          authorization,
+          csrfToken,
+        })
+        // const res = await browser.tabs.sendMessage(tabId, "hi")
+        console.log("res", res)
+        messageSent = true
+        console.log("Sent message")
+      } catch (err) {
+        console.log("error", err)
+        await delay(50)
+      }
+    }
+  }
+
+  // end of script
 }
